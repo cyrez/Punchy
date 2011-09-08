bc4236bbed150b95cea513d8aa2838e2daaa3f13
metti <groebner01@web.de>
2011-09-08 14:06:15 +0200

    fix issue with will_paginate

--- app/controllers/punches_controller.rb
+++ app/controllers/punches_controller.rb
@@ -28,7 +28,7 @@ class PunchesController < ApplicationController
   # GET /punches
   # GET /punches.xml
   def index
-    @punches = Punch.paginate(:page => params[:page], :per_page => Punch.per_page)
+    @punches = Punch.paginate(:page => params[:page], :per_page => Punch.per_page).all
     @punch = Punch.new
 
     respond_to do |format|
